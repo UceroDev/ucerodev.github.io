f6320a136e98c80305d812b9a30fd17556482df1
UceroDev <annaucero2@gmail.com>
2026-06-08 12:56:31 +0200

    Separate projects info to a .ts file

---
+++ version-2/src/data/proyectos.ts
@@ -0,0 +1,14 @@
+export const proyectos = [
+  {
+    titulo: "Web pública e interna de Erasmus",
+    descripcion: "Continuación del desarrollo de la página interna que usa el instituto Nicolau Copèrnic para gestionar las prácticas en el estranjero, aparte de realizar modificaciones necesarias en la web pública.",
+    imagen: "/src/assets/images/p1-erasmus.png",
+    url: "/proyectos/1",
+  },
+  {
+    titulo: "gastroGest",
+    descripcion: "Sistema de gestión de restaurantes con TPV y Kitchen Display.",
+    imagen: "/ruta/imagen2.jpg",
+    url: "/proyectos/2",
+  },
+];
