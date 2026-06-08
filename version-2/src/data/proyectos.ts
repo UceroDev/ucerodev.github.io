844342eacead66740a0d7f39d3ab1be89aa78e77
UceroDev <annaucero2@gmail.com>
2026-06-08 12:57:51 +0200

    Add slug to projects

--- version-2/src/data/proyectos.ts
+++ version-2/src/data/proyectos.ts
@@ -1,11 +1,13 @@
 export const proyectos = [
   {
+    slug: 'erasmus',
     titulo: "Web pública e interna de Erasmus",
     descripcion: "Continuación del desarrollo de la página interna que usa el instituto Nicolau Copèrnic para gestionar las prácticas en el estranjero, aparte de realizar modificaciones necesarias en la web pública.",
     imagen: "/src/assets/images/p1-erasmus.png",
     url: "/proyectos/1",
   },
   {
+    slug: 'gastrogest',
     titulo: "gastroGest",
     descripcion: "Sistema de gestión de restaurantes con TPV y Kitchen Display.",
     imagen: "/ruta/imagen2.jpg",
