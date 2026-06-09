fe19639eb1057797cb4ec803c9452ae2638d3e98
UceroDev <annaucero2@gmail.com>
2026-06-09 09:22:02 +0200

    Separate info of experienca and formacion in a .ts file

---
+++ version-2/src/data/formacion.ts
@@ -0,0 +1,20 @@
+export const formaciones = [
+{
+    fecha: "2024 - 2026",
+    formacion: "CFGS Desarrollo de aplicaciones web",
+    centro: "Instituto Nicolau Copèrnic",
+    lugar: "Terrassa, Barcelona",
+  },
+  {
+    fecha: "2024 - 600 HORAS",
+    formacion: "CP Confección y publicación de páginas web",
+    centro: "Instituto Nicolau Copèrnic",
+    lugar: "Terrassa, Barcelona",
+  },
+  {
+    fecha: "2024 - 600 HORAS",
+    formacion: "CP Confección y publicación de páginas web",
+    centro: "Instituto Nicolau Copèrnic",
+    lugar: "Terrassa, Barcelona",
+  },
+];
