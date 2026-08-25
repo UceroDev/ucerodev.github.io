b702145f172b2c10e8508252e065b0aec42b828a
UceroDev <annaucero2@gmail.com>
2026-08-25 16:00:14 +0200

    create data file with buttons info

---
+++ v3/src/data/btn-info.ts
@@ -0,0 +1,37 @@
+export const proyectos = [
+  {
+    icono: "../assets/images/folder.png",
+    titulo: "Proyectos",
+    url: "",
+  },
+  {
+    icono: "../assets/images/folder.png",
+    titulo: "Formación",
+    url: "",
+  },
+  {
+    icono: "../assets/images/folder.png",
+    titulo: "Sobre mi",
+    url: "",
+  },
+  {
+    icono: "../assets/images/folder.png",
+    titulo: "Diseños",
+    url: "",
+  },
+  {
+    icono: "../assets/svg/github-logo-svg",
+    titulo: "GitHub",
+    url: "https://github.com/UceroDev",
+  },
+  {
+    icono: "../assets/svg/linkedin-logo-svg",
+    titulo: "Linkedin",
+    url: "https://www.linkedin.com/in/anna-ucero-ferre-7a87a4266/",
+  },
+  {
+    icono: "../assets/svg/cv-icon.svg",
+    titulo: "Currículum",
+    url: "https://www.linkedin.com/in/anna-ucero-ferre-7a87a4266/",
+  },
+];
