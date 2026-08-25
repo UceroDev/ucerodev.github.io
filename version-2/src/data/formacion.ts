16f4c8dfbd14cc8af753d567b6e39d443e80e355
UceroDev <annaucero2@gmail.com>
2026-08-25 15:19:32 +0200

    create new v3 folder with astro and tailwind elements

--- version-2/src/data/formacion.ts
+++ version-2/src/data/formacion.ts
@@ -1,13 +1,13 @@
 export const formaciones = [
-{
-    fecha: "2024 - 2026",
-    formacion: "CFGS Desarrollo de aplicaciones web",
+  {
+    fecha: "2026 - 2027",
+    formacion: "Curso Especialización en Ciberseguridad",
     centro: "Instituto Nicolau Copèrnic",
     lugar: "Terrassa, Barcelona",
   },
   {
-    fecha: "2024 - 600 HORAS",
-    formacion: "CP Confección y publicación de páginas web",
+    fecha: "2024 - 2026",
+    formacion: "CFGS Desarrollo de aplicaciones web",
     centro: "Instituto Nicolau Copèrnic",
     lugar: "Terrassa, Barcelona",
   },
