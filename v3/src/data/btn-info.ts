a03bf2c190e2531adfdc50487d7f6ce1e65cda16
UceroDev <annaucero2@gmail.com>
2026-08-25 17:57:19 +0200

    add modalId to buttons

--- v3/src/data/btn-info.ts
+++ v3/src/data/btn-info.ts
@@ -1,4 +1,5 @@
 import folderIcon from "../assets/images/folder.png";
+import fileIcon from "../assets/svg/notes-icon.svg";
 
 export const buttons = [
   {
@@ -6,23 +7,27 @@ export const buttons = [
     titulo: "Proyectos",
     url: "",
     posicion: "left-[40%] top-[22%]",
+    modalId: "proyectos-window",
   },
   {
     icono: folderIcon.src,
     titulo: "Formación",
     url: "",
     posicion: "left-[47%] top-[53%]",
+    modalId: "formacion-window",
   },
   {
     icono: folderIcon.src,
     titulo: "Diseño",
     url: "",
     posicion: "left-[18%] top-[34%]",
+    modalId: "diseno-window",
   },
   {
-    icono: folderIcon.src,
+    icono: fileIcon.src,
     titulo: "Sobre mí",
     url: "",
     posicion: "left-[25%] top-[65%]",
+    modalId: "sobre-mi-window",
   },
 ];
