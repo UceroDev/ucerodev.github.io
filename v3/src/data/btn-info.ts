a91c539aab5accd87001a1d3ea710afc0264114f
UceroDev <annaucero2@gmail.com>
2026-08-25 16:33:52 +0200

    separate folder buttons from social buttons

--- v3/src/data/btn-info.ts
+++ v3/src/data/btn-info.ts
@@ -1,7 +1,4 @@
 import folderIcon from "../assets/images/folder.png";
-import githubIcon from "../assets/svg/github-logo.svg";
-import linkedinIcon from "../assets/svg/linkedin-logo.svg";
-import cvIcon from "../assets/svg/cv-icon.svg";
 
 export const buttons = [
   {
@@ -23,20 +20,5 @@ export const buttons = [
     icono: folderIcon.src,
     titulo: "Sobre mi",
     url: "",
-  },
-  {
-    icono: githubIcon.src,
-    titulo: "GitHub",
-    url: "https://github.com/UceroDev",
-  },
-  {
-    icono: linkedinIcon.src,
-    titulo: "LinkedIn",
-    url: "https://www.linkedin.com/in/anna-ucero-ferre-7a87a4266/",
-  },
-  {
-    icono: cvIcon.src,
-    titulo: "Currículum",
-    url: "...",
   },
 ];
---
+++ v3/src/data/social-info.ts
@@ -0,0 +1,21 @@
+import githubIcon from "../assets/svg/github-logo.svg";
+import linkedinIcon from "../assets/svg/linkedin-logo.svg";
+import cvIcon from "../assets/svg/cv-icon.svg";
+
+export const socials = [
+  {
+    icono: githubIcon.src,
+    titulo: "GitHub",
+    url: "https://github.com/UceroDev",
+  },
+  {
+    icono: linkedinIcon.src,
+    titulo: "LinkedIn",
+    url: "https://www.linkedin.com/in/anna-ucero-ferre-7a87a4266/",
+  },
+  {
+    icono: cvIcon.src,
+    titulo: "Currículum",
+    url: "...",
+  },
+];
