738a2acfcff881171eec96a1a176d9148723b57e
UceroDev <annaucero2@gmail.com>
2026-08-27 12:25:27 +0200

    delete url from buttons, not needed

--- v3/src/data/btn-info.ts
+++ v3/src/data/btn-info.ts
@@ -5,28 +5,24 @@ export const buttons = [
   {
     icono: folderIcon.src,
     titulo: "Proyectos",
-    url: "",
     posicion: "left-[40%] top-[22%]",
     modalId: "proyectos-window",
   },
   {
     icono: folderIcon.src,
     titulo: "Formación",
-    url: "",
     posicion: "left-[47%] top-[53%]",
     modalId: "formacion-window",
   },
   {
     icono: folderIcon.src,
     titulo: "Diseño",
-    url: "",
     posicion: "left-[18%] top-[34%]",
     modalId: "diseno-window",
   },
   {
     icono: fileIcon.src,
     titulo: "Sobre mí",
-    url: "",
     posicion: "left-[25%] top-[65%]",
     modalId: "sobre-mi-window",
   },
